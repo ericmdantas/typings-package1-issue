3be73bf8c583a7512506913f0823d5dbf89a6d24
Eric M. Dantas <ericdantas0@gmail.com>
2020-06-22 16:10:46 -0300

    Change interface

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 export interface MyInterface {
+    name: string;
     doSomething():string;
 }
 
 export class ClassFromPackage1 implements MyInterface {
+    name = "package1";
     doSomething():string {
-        return "hello from package1!";
+        return `hello from ${this.name}!`;
     }
 }
